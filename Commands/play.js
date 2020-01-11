fc5d719f95548fa3e1decef25a18f0cb77f469c3
Alex <aleks.djordjevic5@gmail.com>
2020-01-11 22:36:05 +0100

    Merged youtube search and play commands, added a reply saying whats being played

--- Commands/play.js
+++ Commands/play.js
@@ -1,15 +1,49 @@
+const {GoogleApi} = require("../config.json");
+const play = require("./play")
 const ytdl = require('ytdl-core')
+const search = require('youtube-search');
+const validUrl = require('valid-url');
+
 module.exports = function(args, receivedMessage) {
-    const streamOptions = { seek: 0, volume: 0.3, passes: 1 };
+    const streamOptions = { seek: 0, volume: 0.3, passes: 2 };
     const broadcast = receivedMessage.client.createVoiceBroadcast();
 
-    if(receivedMessage.guild && receivedMessage.member.voiceChannel){
-        receivedMessage.member.voiceChannel.join()
-        .then(connection => {
-            const stream = ytdl(args.join(" "), {filter: 'audioonly'});
-            broadcast.playStream(stream, streamOptions);
-            const dispatcher = connection.playBroadcast(broadcast, streamOptions);
-        })
-        .catch(console.error);
+    if(validUrl.isUri(receivedMessage.content)){
+        if(receivedMessage.guild && receivedMessage.member.voiceChannel){
+            receivedMessage.member.voiceChannel.join()
+            .then(connection => {
+                const stream = ytdl(args.join(" "), {filter: 'audioonly'});
+                broadcast.playStream(stream, streamOptions);
+                const dispatcher = connection.playBroadcast(broadcast, streamOptions);
+                return ytdl.getInfo(args.join(" "));
+            })
+            .then(info => {
+            receivedMessage.reply("Now playing `" + info.title + "`")
+            .catch(console.error);
+            })
+        }
+    }
+    else{
+        const opts = {
+            type:"video",
+            maxResults: 1,
+            key: GoogleApi
+        };
+
+        if(receivedMessage.guild && receivedMessage.member.voiceChannel){
+            Promise.all([receivedMessage.member.voiceChannel.join(), search(receivedMessage.content, opts)])
+            .then(values => {
+                const connection = values[0]
+                const results = values[1].results
+                const stream = ytdl(results[0].link, {filter: 'audioonly'});
+                broadcast.playStream(stream, streamOptions);
+                const dispatcher = connection.playBroadcast(broadcast, streamOptions);
+                return ytdl.getInfo(results[0].link);
+            })
+            .then(info =>{
+                receivedMessage.reply("Now playing `" + info.title + "`")
+                .catch(console.error);
+            })
+        };
     }
 }
--- Commands/youtube-search.js
+++ Commands/youtube-search.js
@@ -1,4 +1,4 @@
-const {GoogleApi} = require("../config.json");
+/*const {GoogleApi} = require("../config.json");
 const play = require("./play")
 const ytdl = require('ytdl-core')
 const search = require('youtube-search');
@@ -12,18 +12,53 @@ module.exports = function(args, receivedMessage) {
   };
 
   if(receivedMessage.guild && receivedMessage.member.voiceChannel){
-    receivedMessage.member.voiceChannel.join()
-    .then(connection => {
-      search(receivedMessage.content, opts, function(err, results){
-        if(err){ 
-          console.log(err);
-          console.dir(results);
-          return
-        }
-        const stream = ytdl(results[0].link, {filter: 'audioonly'});
-        broadcast.playStream(stream, streamOptions);
-        const dispatcher = connection.playBroadcast(broadcast, streamOptions);
-      })
-    });
+    Promise.all([receivedMessage.member.voiceChannel.join(), search(receivedMessage.content, opts)])
+    .then(values => {
+      const connection = values[0]
+      const results = values[1].results
+      const stream = ytdl(results[0].link, {filter: 'audioonly'});
+      broadcast.playStream(stream, streamOptions);
+      const dispatcher = connection.playBroadcast(broadcast, streamOptions);
+      return ytdl.getInfo(results[0].link);
+    })
+    .then(info =>{
+      receivedMessage.reply("Now playing `" + info.title + "`")
+      .catch(console.error);
+    })
   };
 }
+
+/*const {GoogleApi} = require("../config.json");
+const play = require("./play")
+const ytdl = require('ytdl-core')
+const search = require('youtube-search');
+module.exports = function(args, receivedMessage) {
+  const streamOptions = { seek: 0, volume: 0.3, passes: 1 };
+  const broadcast = receivedMessage.client.createVoiceBroadcast();
+  const opts = {
+    type:"video",
+    maxResults: 1,
+    key: GoogleApi
+  };
+
+    if(receivedMessage.guild && receivedMessage.member.voiceChannel){
+      receivedMessage.member.voiceChannel.join()
+      .then(connection => {
+          search(receivedMessage.content, opts, function(err, results){
+          if(err){ 
+            console.log(err);
+            console.dir(results);
+            return
+          }
+          const stream = ytdl(results[0].link, {filter: 'audioonly'});
+          broadcast.playStream(stream, streamOptions);
+          const dispatcher = connection.playBroadcast(broadcast, streamOptions);
+          ytdl.getInfo(results[0].link)
+          .then(info =>{
+            receivedMessage.reply("Now playing `" + info.title + "`")
+            .catch(console.error);
+          })
+        })
+      })
+  };
+}*/

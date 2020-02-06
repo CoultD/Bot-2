a514f317c736a69225effcc199d0e708224855aa
Alex <aleks.djordjevic5@gmail.com>
2020-02-06 01:25:28 +0100

    updated skip

--- Commands/skip.js
+++ Commands/skip.js
@@ -1,6 +1,8 @@
 const play = require("./play")
 module.exports = function(args, message) {
     if(message.guild && message.guild.voiceConnection){
+        if(message.guild && message.member.voiceChannel){
         play.broadcastStream.end()
+        }
     }
 }
